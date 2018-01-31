b2b5179c6162ea120e3c78110c7b6e07acd11dc0
Neža Habjan <nezahabjan123@gmail.com>
2018-01-31 19:44:35 +0100

    shiny prikaz potrošnje po državah

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,24 @@
 library(shiny)
 
+#shinyUI(fluidPage(
+#  
+#  titlePanel("Potrošnja po področjih"),
+#  
+#  tabsetPanel(
+#      tabPanel("Velikost družine",
+#               DT::dataTableOutput("druzine")),
+#      
+#      tabPanel("Število naselij",
+#               sidebarPanel(
+#                  uiOutput("pokrajine")
+#                ),
+#               mainPanel(plotOutput("naselja")))
+#    )
+#))
+
 shinyUI(fluidPage(
-  
-  titlePanel("Slovenske občine"),
-  
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
+  titlePanel("Potrošnja po področjih"),
+  sidebarLayout(
+    sidebarPanel("Izbor države"),
+    mainPanel("Graf potrošnje", img(src="potrosnja.jpg", height=80, weigth=60)))
 ))
